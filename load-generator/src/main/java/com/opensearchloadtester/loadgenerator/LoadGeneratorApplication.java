cee0882482c560b59f6020e9e2195cf2999376fe
Eugen Becker <e.becker.student@gmail.com>
2025-10-29 02:45:18 +0100

    chore: add initial spring boot project for load-generator

---
+++ load-generator/src/main/java/com/opensearchloadtester/loadgenerator/LoadGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.opensearchloadtester.loadgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadGeneratorApplication.class, args);
+	}
+
+}
