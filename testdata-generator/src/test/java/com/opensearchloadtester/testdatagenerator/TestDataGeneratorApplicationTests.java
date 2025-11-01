319f4fa5c6cba4793bc5f72c9d0416439408b341
Leo Hofmann <leohof23@gmail.com>
2025-11-01 23:58:58 +0100

    chore: Created initial folder structure and empty spring boot app for load-generator.
    Signed-off-by: Leo Hofmann <leohof23@gmail.com>

---
+++ testdata-generator/src/test/java/com/opensearchloadtester/testdatagenerator/TestDataGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opensearchloadtester.testdatagenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDataGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
