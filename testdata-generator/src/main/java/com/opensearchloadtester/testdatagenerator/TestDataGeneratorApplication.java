1fd02c0f00b84cc426b7563742608b4becae5b3f
sebastian <sebastian.alxr@gmail.com>
2025-11-02 21:27:27 +0100

    main graceful exit handling for testing / showcase
    Signed-off-by: sebastian <sebastian.alxr@gmail.com>

--- testdata-generator/src/main/java/com/opensearchloadtester/testdatagenerator/TestDataGeneratorApplication.java
+++ testdata-generator/src/main/java/com/opensearchloadtester/testdatagenerator/TestDataGeneratorApplication.java
@@ -2,12 +2,25 @@ package com.opensearchloadtester.testdatagenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class TestDataGeneratorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestDataGeneratorApplication.class, args);
+
+		ConfigurableApplicationContext ctx = SpringApplication.run(TestDataGeneratorApplication.class, args);
+
+
+		try {
+			Thread.sleep(10000); // 10 seconds
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+
+		// exit gracefully with code 0 to indicate success (so Compose sees service_completed_successfully)
+		int exitCode = SpringApplication.exit(ctx, () -> 0);
+		System.exit(exitCode);
 	}
 
 }
