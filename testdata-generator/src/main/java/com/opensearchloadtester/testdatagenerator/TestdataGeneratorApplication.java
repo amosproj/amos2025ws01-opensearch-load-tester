40d2861807cddc324e9d612ffe6e961b54d39c9e
Eugen Becker <e.becker.student@gmail.com>
2025-11-03 15:19:45 +0100

    Chore: add spring boot project for testdata-generator
    Co-authored-by: Leo Hofmann <leohof23@gmail.com>
    Signed-off-by: Eugen Becker <e.becker.student@gmail.com>

---
+++ testdata-generator/src/main/java/com/opensearchloadtester/testdatagenerator/TestdataGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.opensearchloadtester.testdatagenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdataGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestdataGeneratorApplication.class, args);
+	}
+
+}
