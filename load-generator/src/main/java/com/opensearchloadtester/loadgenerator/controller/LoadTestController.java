74e6c84f621bf85fdf7962d1469e32f2bc964d38
Carlit0 <carlostrachwitz@googlemail.com>
2025-11-07 18:38:05 +0100

    feat: add LoadTestController REST interface
    Signed-off-by: Carlit0 <carlostrachwitz@googlemail.com>

---
+++ load-generator/src/main/java/com/opensearchloadtester/loadgenerator/controller/LoadTestController.java
@@ -0,0 +1,56 @@
+package com.opensearchloadtester.loadgenerator.controller;
+
+import com.opensearchloadtester.loadgenerator.runner.LoadRunnerService;
+import com.opensearchloadtester.loadgenerator.runner.NoOpQueryExecution;
+import com.opensearchloadtester.loadgenerator.runner.QueryExecutionFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * REST Controller for triggering load tests.
+ */
+@Slf4j
+@RestController
+@RequestMapping("/api/load-test")
+public class LoadTestController {
+
+    private final LoadRunnerService loadRunnerService;
+
+    public LoadTestController(LoadRunnerService loadRunnerService) {
+        this.loadRunnerService = loadRunnerService;
+    }
+
+    /**
+     * Starts a load test with n parallel query execution threads.
+     * 
+     * @param threadCount Number of parallel threads to spawn
+     * @return Response indicating success or failure
+     */
+    @PostMapping("/start")
+    public ResponseEntity<String> startLoadTest(@RequestParam(defaultValue = "5") int threadCount) {
+        log.info("Received request to start load test with {} threads", threadCount);
+
+        try {
+            // Create query executions using factory pattern
+            QueryExecutionFactory factory = threadId -> 
+                new NoOpQueryExecution("query-" + threadId, 1000);
+
+            loadRunnerService.executeQueries(threadCount, factory);
+
+            return ResponseEntity.ok(
+                String.format("Load test completed successfully with %d threads", threadCount)
+            );
+        } catch (InterruptedException e) {
+            log.error("Load test was interrupted", e);
+            Thread.currentThread().interrupt();
+            return ResponseEntity.status(500)
+                .body("Load test was interrupted: " + e.getMessage());
+        } catch (Exception e) {
+            log.error("Error executing load test", e);
+            return ResponseEntity.status(500)
+                .body("Error executing load test: " + e.getMessage());
+        }
+    }
+
+
