7d86c6c3078265c4cc02a70d277ec0030649c49b
Eugen Becker <e.becker.student@gmail.com>
2025-11-13 14:41:03 +0100

    feat: add spring-boot-starter-validation to pom.xml and enforce min count for test data objects
    Signed-off-by: Eugen Becker <e.becker.student@gmail.com>

--- testdata-generator/src/main/java/com/opensearchloadtester/testdatagenerator/config/DataGenerationProperties.java
+++ testdata-generator/src/main/java/com/opensearchloadtester/testdatagenerator/config/DataGenerationProperties.java
@@ -1,9 +1,12 @@
 package com.opensearchloadtester.testdatagenerator.config;
 
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "data.generation")
+@Validated
 @Data
 public class DataGenerationProperties {
 
@@ -13,5 +16,6 @@ public class DataGenerationProperties {
 
     private Mode mode;
     private String outputPath;
+    @Min(1)
     private int count;
 }
