06ea0c35d3ba4f1edd23758bb73ad09177aa45c2
Leo Hofmann <leohof23@gmail.com>
2025-11-17 15:05:57 +0100

    feat: Implemented MetricsReporterClient Added DTO class Metrics to hold metrics data. Added method reportMetrics - sends HTTP request to MetricsReporter. Added property "metrics-reporter.url".
    Signed-off-by: Leo Hofmann <leohof23@gmail.com>

---
+++ load-generator/src/main/java/com/opensearchloadtester/loadgenerator/dto/Metrics.java
@@ -0,0 +1,50 @@
+package com.opensearchloadtester.loadgenerator.dto;
+
+import lombok.Data;
+
+@Data
+public class Metrics {
+
+    /**
+     * loadGeneratorInstance   Name of reporting loadGenerator instance
+     * requestType          Type of query that was executed
+     * roundtripMilSec      Time from first request to final answer
+     * jsonResponse         Response of query execution
+     */
+    private String loadGeneratorInstance;
+    private String requestType;
+    private int roundtripMilSec;
+    private String jsonResponse;
+
+    public String getLoadGeneratorInstance() {
+        return loadGeneratorInstance;
+    }
+
+    public void setLoadGeneratorInstance(String loadGeneratorInstance) {
+        this.loadGeneratorInstance = loadGeneratorInstance;
+    }
+
+    public String getRequestType() {
+        return requestType;
+    }
+
+    public void setRequestType(String requestType) {
+        this.requestType = requestType;
+    }
+
+    public int getRoundtripMilSec() {
+        return roundtripMilSec;
+    }
+
+    public void setRoundtripMilSec(int roundtripMilSec) {
+        this.roundtripMilSec = roundtripMilSec;
+    }
+
+    public String getJsonResponse() {
+        return jsonResponse;
+    }
+
+    public void setJsonResponse(String jsonResponse) {
+        this.jsonResponse = jsonResponse;
+    }
+}
