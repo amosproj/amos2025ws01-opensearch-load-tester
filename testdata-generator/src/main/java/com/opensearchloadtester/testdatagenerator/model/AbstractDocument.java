b3974159fbefdfe2acba69c1193a85e36255c14a
Sebastian <sebastian.alxr@gmail.com>
2025-12-01 19:22:32 +0100

    feat: update ano data generation w more realisitc data
    Signed-off-by: Sebastian <sebastian.alxr@gmail.com>

--- testdata-generator/src/main/java/com/opensearchloadtester/testdatagenerator/model/AbstractDocument.java
+++ testdata-generator/src/main/java/com/opensearchloadtester/testdatagenerator/model/AbstractDocument.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import java.time.Instant;
 import java.util.Random;
+import java.util.UUID;
 
 /**
  * Base class for OpenSearch document data.
@@ -38,22 +39,28 @@ public abstract class AbstractDocument implements Document {
 
 
     protected static <T extends AbstractDocument> T fillCommonFieldsRandomly(T document) {
-        // Fill with random values without purpose
-        document.id = "id-" + RANDOM.nextInt(100000);
+        // Fill with random values
+        document.id = UUID.randomUUID().toString() + "_" + UUID.randomUUID().toString();
         document.customAll = "This is customAll from " + document.id;
-        document.contentLength = RANDOM.nextLong(10000);
-        document.contentType = "Document";
-        document.dssCreationUserDisplayName = "RandomGenerator";
-        document.dssCreationUserIdKey = "user-random";
-        document.dssDataspaceId = "dataspace-" + RANDOM.nextInt(1001);
-        document.dssDocumentId = "document-" + RANDOM.nextInt(10000);
-        document.dssDocumentName = "Payroll-" + document.dssDocumentId + "-random";
+        document.contentLength = RANDOM.nextLong(20000, 30000);
+        document.contentType = "application/pdf";
+        document.dssCreationUserDisplayName = "";
+        document.dssCreationUserIdKey = UUID.randomUUID().toString();
+        // first part of id
+        document.dssDataspaceId = document.id.substring(0, document.id.indexOf('_'));
+        // last part of id
+        document.dssDocumentId =  document.id.substring(document.id.indexOf('_') + 1);
+
+        // TODO: Hilfe :((
+        document.dssDocumentName = "Brutto-Netto-Abrechnung " + document.dssDocumentId + ".pdf";
+
         document.dssLastModifiedDatetime = Instant.now();
-        document.dssLastModifiedUserDisplayName = "RandomGenerator";
-        document.dssLastModifiedUserIdKey = "user-random";
+        document.dssLastModifiedUserDisplayName = "";
+        document.dssLastModifiedUserIdKey = UUID.randomUUID().toString();
         document.dssProcessingFlagOwner = "owner-random";
-        document.dssVersion = "1.0";
-        document.etag = "etag-" + RANDOM.nextInt(100000000);
+        document.dssVersion = "1." + RANDOM.nextInt(9);
+        document.etag = UUID.randomUUID().toString().replace("-", "");
+        // irrelevant, bc not queried on
         document.lastDocumentChange = Instant.now();
         return document;
     }
--- testdata-generator/src/main/java/com/opensearchloadtester/testdatagenerator/model/ano/AnoDocument.java
+++ testdata-generator/src/main/java/com/opensearchloadtester/testdatagenerator/model/ano/AnoDocument.java
@@ -5,10 +5,12 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.Instant;
 import java.util.List;
 
+@Slf4j
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PRIVATE) // AnoDocument objects must be created via random() method
@@ -28,22 +30,46 @@ public class AnoDocument extends AbstractDocument {
     private String dssOriginalFilename;
     private Boolean dssRecyclebin;
 
-
     // Method to create a random AnoDocument object
     public static AnoDocument random() {
         AnoDocument anoDocument = fillCommonFieldsRandomly(new AnoDocument());
 
-        // Fill with random values without purpose
-        anoDocument.dssCreationDatetime = Instant.now();
+        // Fill with random values
         anoDocument.dssCustomMetadataPayrollInfo = PayrollInfo.random();
-        anoDocument.dssDeleteRetentionMinRetention = Instant.now();
-        anoDocument.dssDocumentOrientation = RANDOM.nextLong(4);
-        anoDocument.dssDocumentPath = "example/home/payrolls/" + anoDocument.dssDocumentName;
-        anoDocument.dssDocumentSource = "https://example.de/payrolls/" + anoDocument.dssDocumentName;
+        // irrelevant, bc not queried on
+        anoDocument.dssCreationDatetime = Instant.now();
+        // All examples where null
+        anoDocument.dssDeleteRetentionMinRetention = null;
+        // All examples were 0
+        anoDocument.dssDocumentOrientation = (long) 0;
+        anoDocument.dssDocumentPath = "/" + anoDocument.dssDataspaceId
+                + "/documents/" + anoDocument.dssDocumentId + "/document";
+        anoDocument.dssDocumentSource = null;
+        // irrelevant, bc not queried on
         anoDocument.dssLastModifiedUserDatetime = Instant.now();
-        anoDocument.dssOriginalFilename = anoDocument.dssDocumentName;
+        anoDocument.dssOriginalFilename = "Brutto-Netto-Abrechnung "
+                + translateMonthToName(anoDocument.dssCustomMetadataPayrollInfo.getAccountingMonth())
+                + " " + anoDocument.dssCustomMetadataPayrollInfo.getAccountingYear() + ".pdf";
         anoDocument.dssRecyclebin = false;
         return anoDocument;
+    }
+
+    public static String translateMonthToName(int month) {
+        return switch (month) {
+            case 1 -> "Januar";
+            case 2 -> "Februar";
+            case 3 -> "März";
+            case 4 -> "April";
+            case 5 -> "Mai";
+            case 6 -> "Juni";
+            case 7 -> "Juli";
+            case 8 -> "August";
+            case 9 -> "September";
+            case 10 -> "Oktober";
+            case 11 -> "November";
+            case 12 -> "Dezember";
+            default -> "Unknown";
+        };
     }
 
     /**
@@ -65,15 +91,38 @@ public class AnoDocument extends AbstractDocument {
         public static PayrollInfo random() {
             PayrollInfo payrollInfo = new PayrollInfo();
 
-            // Fill with random values without purpose
-            payrollInfo.accountingMonth = RANDOM.nextInt(12) + 1;
-            payrollInfo.accountingYear = 2020 + RANDOM.nextInt(6);
-            payrollInfo.firstAccess = Instant.now();
+            // Example query was from 2018 to 2024 --> 7 years
+            // We generate from 2017 to 2025 -> 10 years for ~20% misses
+            java.time.LocalDate startDate = java.time.LocalDate.of(2017, 1, 1);
+            java.time.LocalDate endDate = java.time.LocalDate.of(2025, 12, 31);
+            Instant start = startDate.atStartOfDay(java.time.ZoneId.systemDefault()).toInstant();
+            Instant end = endDate.atTime(23, 59, 59).atZone(java.time.ZoneId.systemDefault()).toInstant();
+            long startSec = start.getEpochSecond();
+            long endSec = end.getEpochSecond();
+            long randomSec = java.util.concurrent.ThreadLocalRandom.current().nextLong(startSec, endSec + 1);
+            payrollInfo.provisionDate = Instant.ofEpochSecond(randomSec);
+            log.info("provisionDate: {}", payrollInfo.provisionDate);
+
+            // Extract month and year from provisionDate
+            java.time.ZonedDateTime zdt = payrollInfo.provisionDate.atZone(java.time.ZoneId.systemDefault());
+            payrollInfo.accountingMonth = zdt.getMonthValue();
+            payrollInfo.accountingYear = zdt.getYear();
+
+            // can be null (30%) or typically next month (16 days later)
+            if (RANDOM.nextDouble() < 0.3) {
+                payrollInfo.firstAccess = null;
+            } else {
+                // 16 days == 23040 Minutes
+                int randomMinutes = java.util.concurrent.ThreadLocalRandom.current().nextInt(1, 23040 + 1);
+                Instant potentiallyFirstAccess = payrollInfo.provisionDate;
+                payrollInfo.firstAccess = potentiallyFirstAccess.plus(java.time.Duration.ofMinutes(randomMinutes));
+            }
+
             List<String> lang = List.of("German", "English", "Spanish", "French");
             payrollInfo.language = lang.get(RANDOM.nextInt(lang.size()));
             List<String> type = List.of("Monthly", "Yearly", "Quarterly");
             payrollInfo.payrollType = type.get(RANDOM.nextInt(type.size()));
-            payrollInfo.provisionDate = Instant.now();
+
             return payrollInfo;
         }
     }
