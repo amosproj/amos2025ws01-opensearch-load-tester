87dfcaab6708fc6287a7cbd19aedc965f4d49a97
Carlit0 <carlostrachwitz@googlemail.com>
2025-12-06 15:21:22 +0700

    refactor: keep metricsList internal
    Signed-off-by: Carlit0 <carlostrachwitz@googlemail.com>

--- common-core/src/main/java/com/opensearchloadtester/common/dto/LoadGeneratorReportDto.java
+++ common-core/src/main/java/com/opensearchloadtester/common/dto/LoadGeneratorReportDto.java
@@ -1,6 +1,5 @@
 package com.opensearchloadtester.common.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,13 +10,18 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class LoadGeneratorReportDto {
 
     private String loadGeneratorId;
     private String scenario;
     private String queryType;
     private final List<MetricsDto> metricsList = new ArrayList<>();
+
+    public LoadGeneratorReportDto(String loadGeneratorId, String scenario, String queryType) {
+        this.loadGeneratorId = loadGeneratorId;
+        this.scenario = scenario;
+        this.queryType = queryType;
+    }
 
     public synchronized void addMetrics(MetricsDto metricsDto) {
         metricsList.add(metricsDto);
