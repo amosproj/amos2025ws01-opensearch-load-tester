18debb2ef3035fac3d50e4fff9f03f24502d7b87
Carlit0 <carlostrachwitz@googlemail.com>
2026-01-26 16:54:28 +0100

    feat: added ANO dis_max query
    Signed-off-by: Carlit0 <carlostrachwitz@googlemail.com>

--- load-generator/src/main/java/com/opensearchloadtester/loadgenerator/model/QueryType.java
+++ load-generator/src/main/java/com/opensearchloadtester/loadgenerator/model/QueryType.java
@@ -11,6 +11,7 @@ public enum QueryType {
     DUO_BOOKING_BY_CLIENT_AND_STATE(DuoBookingByClientAndStateQuery::random),
     ANO_CLIENTS_AGGREGATION(AnoClientsAggregationQuery::random),
     ANO_CLIENT_BY_YEAR(AnoClientByYearQuery::random),
+    ANO_DIS_MAX(AnoDisMaxQuery::random),
     DUO_CLIENT_BY_CUSTOMER_NUMBER(DuoClientByCustomerNumberQuery::random),
     DUO_CLIENT_BY_NAME_AND_STATE(DuoClientByNameAndStateQuery::random),
     ANO_PAYROLL_TYPE_LANGUAGE(AnoPayrollTypeLanguageQuery::random),
