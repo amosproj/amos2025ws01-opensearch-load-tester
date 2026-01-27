b6ecbf5f6c1ed680cace1a38b9cbc974ce7fa903
Carlit0 <carlostrachwitz@googlemail.com>
2026-01-28 00:16:20 +0100

    refactor(common-core): replace manual validation with Jakarta Bean Validation
    this is needed for easier metric-reporter validation
    
    Signed-off-by: Carlit0 <carlostrachwitz@googlemail.com>

--- common-core/src/main/java/com/opensearchloadtester/common/dto/MetricsDto.java
+++ common-core/src/main/java/com/opensearchloadtester/common/dto/MetricsDto.java
@@ -1,5 +1,8 @@
 package com.opensearchloadtester.common.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +14,21 @@ import lombok.Setter;
 @AllArgsConstructor
 public class MetricsDto {
 
+    @NotBlank
     private String loadGeneratorId;
+
+    @NotBlank
     private String queryType;
+
+    @Min(0)
     private Long requestDurationMillis;
+
+    @Min(0)
     private Long queryDurationMillis;
+
     private Integer totalHits;
+
+    @Min(100)
+    @Max(599)
     private int httpStatusCode;
 }
