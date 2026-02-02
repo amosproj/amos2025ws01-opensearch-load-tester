158b18ccdc01e8e610d67585e6ebda3bc167c683
Eugen Becker <e.becker.student@gmail.com>
2026-02-02 11:06:28 +0100

    refactor(shared-library): remove unused Lombok annotations from DTOs
    Signed-off-by: Eugen Becker <e.becker.student@gmail.com>

--- common-core/src/main/java/com/opensearchloadtester/common/dto/LoadTestStartSyncStatusDto.java
+++ common-core/src/main/java/com/opensearchloadtester/common/dto/LoadTestStartSyncStatusDto.java
@@ -2,12 +2,8 @@ package com.opensearchloadtester.common.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
-@NoArgsConstructor
 @AllArgsConstructor
 public class LoadTestStartSyncStatusDto {
 
--- common-core/src/main/java/com/opensearchloadtester/common/dto/MetricsDto.java
+++ common-core/src/main/java/com/opensearchloadtester/common/dto/MetricsDto.java
@@ -4,12 +4,8 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
-@NoArgsConstructor
 @AllArgsConstructor
 public class MetricsDto {
 
